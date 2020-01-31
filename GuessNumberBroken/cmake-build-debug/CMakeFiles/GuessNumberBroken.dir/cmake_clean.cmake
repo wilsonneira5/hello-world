4a3d0a656c219fcf1daa7816e6137e506d00ff44
wilsonneira5 <58145303+wilsonneira5@users.noreply.github.com>
2020-01-30 20:38:58 -0500

    20200130
    Fixed GuessNumber C++

---
+++ GuessNumberBroken/cmake-build-debug/CMakeFiles/GuessNumberBroken.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GuessNumberBroken.dir/main.cpp.obj"
+  "GuessNumberBroken.pdb"
+  "GuessNumberBroken.exe"
+  "GuessNumberBroken.exe.manifest"
+  "libGuessNumberBroken.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GuessNumberBroken.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
